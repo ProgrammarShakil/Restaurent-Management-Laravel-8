2ade55d27d693a7dc15c3db8adc189242f49a5db
shakil islam <freelancershakil913@gmail.com>
2021-10-04 21:46:07 +0600

    till admin setup

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', [HomeController::class, "index"]);
+Route::get('/', [HomeController::class, "index"]);
+Route::get('/redirects', [HomeController::class, "redirects"]);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
